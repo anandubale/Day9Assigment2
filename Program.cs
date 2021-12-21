b322ff3eeaff1eca2fb91d99955551fbbb93de16
anandubale <au26199@gmail.com>
2021-12-21 15:57:12 +0530

    Added file which create contacts in Address book

--- Program.cs
+++ Program.cs
@@ -7,6 +7,52 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Adress Book PRogram in master Branch");
+
+            ToGetFullName myname = new ToGetFullName();
+            
+            FullAdress myadd = new FullAdress();
+            MobileNumber mymob = new MobileNumber();
+            EMailId myemail = new EMailId();
+
+            
+
+            for (int user = 1; user <= 10; user++)
+            {
+                Console.WriteLine("User " + user + ":");
+
+                //------------------------------------------
+                Console.WriteLine("Enter your First Name");
+                String FirstName = Console.ReadLine();
+                Console.WriteLine("Enter your Last Name");
+                String LastName = Console.ReadLine();
+
+                //------------------------------------------- 
+                
+                Console.WriteLine("Enter your Address");
+                String Address = Console.ReadLine();
+                Console.WriteLine("Enter your city Name");
+                String City = Console.ReadLine();
+                Console.WriteLine("Enter your state");
+                String state = Console.ReadLine();
+                Console.WriteLine("Enter your zip code");
+                String zip = Console.ReadLine();
+
+                //---------------------------------------------
+                Console.WriteLine("Enter your Mobile NUmber");
+                String MobileNo = Console.ReadLine();
+                Console.WriteLine("Enter your Email id");
+                String EmailId = Console.ReadLine();
+
+                
+
+                Console.WriteLine("Full Name: " + ToGetFullName.FirstName(FirstName) + " " + ToGetFullName.LastName(LastName));
+                
+                Console.WriteLine("Address: " + FullAdress.ADDRESS(Address) + " " + FullAdress.CITY(City) + " " + FullAdress.STATE(state) + " " + FullAdress.ZIP(zip));
+                Console.WriteLine("Mobile NUmber: " + MobileNumber.MOBILENUBER(MobileNo));
+                Console.WriteLine("Email ID: " + EMailId.EMAILID(EmailId));
+                Console.WriteLine("");
+                
+            }
         }
     }
 }
---
+++ TocreateContacts.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    
+    
+        class ToGetFullName
+        {
+
+            public static string FirstName(string First_Name)
+            {
+                string FirstName = First_Name;
+                return FirstName;
+            }
+
+            public static string LastName(string Last_Name)
+            {
+                string LastName = Last_Name;
+                return LastName;
+            }
+
+
+        }
+
+    class FullAdress 
+    {
+        public static string ADDRESS(string Address)
+        {
+            string ADDRESS = Address;
+            return ADDRESS;
+        }
+        public static string CITY(string City)
+        {
+            string CITY = City;
+            return CITY;
+        }
+        public static string STATE(string State)
+        {
+            string STATE = State;
+            return STATE;
+
+        }
+        public static string ZIP(string zip)
+        {
+            string ZIP = zip;
+            return ZIP;
+        }
+
+    }
+
+    class MobileNumber
+
+    {
+        public static string MOBILENUBER(string Mobile_number)
+        {
+            string MobileNumber = Mobile_number;
+            return MobileNumber;
+        }
+    }
+
+
+    class EMailId 
+
+    {
+        public static string EMAILID(string EMailId)
+        {
+            string EMAILID = EMailId;
+            return EMAILID;
+            ;
+        }
+    }
+
+}
